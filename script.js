61e2ce6b6eca472be17e4d91b9a6a0536fb2d000
BreakerSneaker <brann1802@gmail.com>
2023-09-15 13:08:27 +0300

    index

---
+++ script.js
@@ -0,0 +1,7 @@
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    // true for mobile device
+    window.open('test.html')
+  }else{
+    // false for not mobile device
+    
+  }
