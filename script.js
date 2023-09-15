c48329631339f5949ce28cda905f73be700abee6
BreakerSneaker <brann1802@gmail.com>
2023-09-15 14:59:51 +0300

    index

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 function isMobile(){
-  return /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+  return navigator.maxTouchPoints>=0 && /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
 
 }
    
 
 if(isMobile()==false){
 
-  window.open('https://breakersneaker.github.io/portfolio/')
-
+  window.open('https://breakersneaker.github.io/portfolio/');
+  close();
 }
-
-   
  
