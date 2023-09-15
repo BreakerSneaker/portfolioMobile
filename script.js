5a4375446ca5f73166e8c8129b16c53d2e551b3c
BreakerSneaker <brann1802@gmail.com>
2023-09-15 13:54:28 +0300

    index

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     // true for mobile device
-   
-  }else{
-    // false for not mobile device
     window.open('https://breakersneaker.github.io/portfolio/')
+   
   }
+
+  
